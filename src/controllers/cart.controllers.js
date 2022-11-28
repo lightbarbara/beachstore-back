92c50a61de444451900b9533664fca17a777c467
Barbara Luz <barbaraluz@eq.ufrj.br>
2022-11-28 17:39:35 -0300

    fix: delete product from cart

--- src/controllers/cart.controllers.js
+++ src/controllers/cart.controllers.js
@@ -1,3 +1,4 @@
+import { ObjectId } from "mongodb"
 import { cartsCollection, salesCollection } from "../database/db.js"
 
 export function getCart(req, res) {
@@ -44,7 +45,32 @@ export async function deleteCart(req, res) {
 
 export async function deleteItem(req, res) {
 
+    const session = res.locals.session
 
+    const {id} = req.params
+
+    try {
+
+        const cart = await cartsCollection.findOne({ userId: session.userId })
+
+        const productToBeRemoved = cart.products.findIndex(p => p._id === id)
+
+        let products = [...cart.products]
+
+        products.splice(productToBeRemoved, 1)
+
+        await cartsCollection.updateOne({ userId: session.userId }, {
+            $set: {
+                products
+            }
+        })
+
+        return res.sendStatus(200)
+
+    } catch (err) {
+        console.log(err)
+        res.sendStatus(500)
+    }
 
 }
 
